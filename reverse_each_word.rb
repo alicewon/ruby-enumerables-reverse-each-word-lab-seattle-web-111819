76e8006c7823113c1effcb52037cdebfd5b41892
Alice Won <alicewon4@gmail.com>
2019-11-14 23:56:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,8 @@ def reverse_each_word(variable_sentance)
   #i=0
   puts "variable_sentance"
   puts variable_sentance
-  variable_sentance.split(" ")#.collect{|word| word.reverse}
-  puts "variable_sentance after enumerable work"
+  variable_sentance.split#.collect{|word| word.reverse}
+  puts "variable_sentance SHOULD RETURN AN ARRAY"
   puts variable_sentance
   # while i < sentence1.length do
   #   sentence1.reverse_each{|word| }

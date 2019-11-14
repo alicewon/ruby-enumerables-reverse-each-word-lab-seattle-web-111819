1bf39312b933238b22edfcb55264a8e04991faa8
Alice Won <alicewon4@gmail.com>
2019-11-14 23:45:59 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+
+  #reverse each word in it's place
+  #can't use an enumerator on a string, so how to turn string into array?
+  #reverse each word and return those altered words?
+  #.each returns the original array but other enumerators don't
+
+def reverse_each_word(sentence1)
+  i=0
+  sentence1.split(" ")
+  # while i < sentence1.length do
+  #   sentence1.reverse_each{|word| }
+  # end
+end

2b324f27016ff5dd06236277802a452fef1a4766
Alice Won <alicewon4@gmail.com>
2019-11-14 23:51:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@
   #reverse each word and return those altered words?
   #.each returns the original array but other enumerators don't
 
-def reverse_each_word(sentence1)
+def reverse_each_word(variable_sentance)
   #i=0
-  sentence1.split(" ").collect {|word| word.reverse_each}
+  variable_sentance.split(" ").collect {|word| word.reverse_each}
 
   # while i < sentence1.length do
   #   sentence1.reverse_each{|word| }
